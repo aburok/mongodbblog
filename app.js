7563c6783c7ff9f53886749d5471542dfddb7ce4
gringo <gringo.kromka@gmail.com>
2014-01-26 12:29:59 +0100

    adding query find with toArray

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/test', function(err,db) {
+var server = 'mongodb://localhost:27017/test';
+var collection = 'scores';
+
+MongoClient.connect(server, function(err,db) {
+	if (err) throw err;
+
+	var query = { score : { $gt : 95 }, type : "exam" };
+
+	db.collection('scores').findOne(query, function(err, doc){
 		if (err) throw err;
 
-		var query = { score : { $gt : 95 }, type : "exam" };
+		console.dir(doc);
 
-		db.collection('scores').findOne(query, function(err, doc){
+		db.close();
+	});
+});
+
+MongoClient.connect(server, function(err, db) {
+	if (err) throw err;
+
+	var query = { $or : [ { score : { $gt : 95 }, type : 'exam' },
+		{score : { $gt : 98 }, type : 'quiz' } ] };
+
+	db.collection('scores').find( query ). toArray( 
+		function(err, docs) {
 			if (err) throw err;
 
-			console.dir(doc);
+			console.dir(docs);
 
 			db.close();
 		});
-		});
-
+});
