3386239618ed903475535e422f6438ca3d6d8e3f
gringo <gringo.kromka@gmail.com>
2014-01-26 12:55:22 +0100

    add consol.log

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ var collection = 'scores';
 MongoClient.connect(server, function(err,db) {
 	if (err) throw err;
 
+
 	var query = { score : { $gt : 95 }, type : "exam" };
 
 	db.collection('scores').findOne(query, function(err, doc){
 		if (err) throw err;
 
+		console.log("Find first student with exam score higher than 95.");
 		console.dir(doc);
 
 		db.close();
@@ -20,13 +22,15 @@ MongoClient.connect(server, function(err,db) {
 MongoClient.connect(server, function(err, db) {
 	if (err) throw err;
 
+
 	var query = { $or : [ { score : { $gt : 95 }, type : 'exam' },
 		{score : { $gt : 98 }, type : 'quiz' } ] };
 
-	db.collection('scores').find( query ). toArray( 
+	db.collection('scores').find( query ).limit(3).toArray( 
 		function(err, docs) {
 			if (err) throw err;
 
+			console.log("Find 3 student with exam score higher than 95 or quiz score higher than 98");
 			console.dir(docs);
 
 			db.close();
