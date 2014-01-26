d2e5a0bc4b81d476158fdae77acaf0bc72095a75
gringo <gringo.kromka@gmail.com>
2014-01-26 12:14:14 +0100

    ignore files ending with ~

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/test', function(err,db) {
-	if (err) throw err;
-
-	var query = { score : 100 };
-
-	db.collection('scores').findOne(query, function(err, doc){
 		if (err) throw err;
 
-		console.dir(doc);
+		var query = { score : { $gt : 95 }, type : "exam" };
 
-		db.close();
-	});
-});
+		db.collection('scores').findOne(query, function(err, doc){
+			if (err) throw err;
+
+			console.dir(doc);
+
+			db.close();
+		});
+		});
 
